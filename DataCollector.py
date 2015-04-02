fc1e12b81df61f88e0bd313e6027eb90bfb8af06
windygyt <windygyt@gmail.com>
2015-04-02 09:30:40 -0400

    remove old zip-county file which has wrong zip code info. Fix the problem different file sharing different naming style for counties. Add a zip table to db.

--- DataCollector.py
+++ DataCollector.py
@@ -8,87 +8,102 @@ import sqlite3
 from os import listdir
 import json
 import os
+import csv
+
+def initDataBase(c):
+    SetupDataBase.createTables(c);
+    SetupDataBase.buildStatesTable(c);
+    SetupDataBase.buildCountiesTable(c);
+    SetupDataBase.buildDemoTables(c);
+    SetupDataBase.buildZipTable(c)
+
+
+
+def queryCounty(c, query_state,query_county):
+
+    publisher = 1 # replace with your own publisher id
+    restaurant_file_folder = 'json/' + query_county + 'County ' + query_state + '/'
+    review_file_folder = 'json/' + query_county + 'County ' + query_state + ' reviews/'
+    query_size = 50 # 50 is the maximum
+
+    collect_data_online = False
+
+    # Step1: using zip code searching for all restaurants and find their id
+    # Example: https://api.citygridmedia.com/content/places/v2/search/where?type=restaurant&where=90045&publisher=xxxx
+    # Step2: using id to find all reviews for that restaurant
+    # Example: https://api.citygridmedia.com/content/reviews/v2/search/where?listing_id=44537579&publisher=xxxx
+    # Note: searching is based on radius on distance, which means using zip does not mean all the result restaurants'
+    # addresses match the zip,
+
+    # county id 1001 Autauga County
+    # zips = [36003, 36006, 36008, 36051, 36066, 36067, 36068, 36749]
+
+    # Info: sqlite date: https://www.sqlite.org/lang_datefunc.html
+
+
+
+
+    if not os.path.exists(restaurant_file_folder):
+        os.makedirs(restaurant_file_folder)
+    if not os.path.exists(review_file_folder):
+        os.makedirs(review_file_folder)
+
+
+    zips = RestaurantCollector.getZips(query_state, query_county)
+    print zips
+    # save all restaurants information to Json files according to their zip code
+    # If interrupted, try restart from a certain zip code
+    if collect_data_online:
+        RestaurantCollector.queryAndSaveRestaurantsJsonFiles(zips, restaurant_file_folder, query_size, publisher)
+
+
+    #initDataBase(c) # call only when recreating the database from empty
+
+    # Parse the restaurants Json file and insert contents into db
+    county_id = SetupDataBase.getCountyID(c, query_state, query_county)
+    print('county id: ' + str(county_id))
+    for file_name in listdir(restaurant_file_folder):
+        with open(restaurant_file_folder + file_name) as json_file:
+            data = json.load(json_file)
+            zip = int(file_name[0:file_name.find('-')])
+            RestaurantJsonFileParser.parseRestaurantJsonFiles(c, data, zip, county_id)
+            print("Finish parsing " + file_name)
+
+    # Gather the restaurant list and get reviews for each one, then save them to Json files
+    if collect_data_online:
+        ReviewCollector.queryAndSaveReviewJsonFiles(c, review_file_folder, county_id, query_size, publisher)
+
+    # Parse the reviews Json file and insert contents into db
+    for file_name in listdir(review_file_folder):
+        with open(review_file_folder + file_name) as json_file:
+            data = json.load(json_file)
+            zip = int(file_name[0:file_name.find('-')])
+            ReviewJsonFileParser.parseReviewJsonFiles(c, data)
+            print("Finish parsing " + file_name)
+
+
 
 # Only change the following 2 param when query different county
 query_state = 'TN' # match zip-county.csv file
 query_county = 'Overton' # match zip-county.csv file
-
-
-publisher = xxxxxxx # replace with your own publisher id
-restaurant_file_folder = 'json/' + query_county + 'County ' + query_state + '/'
-review_file_folder = 'json/' + query_county + 'County ' + query_state + ' reviews/'
-query_size = 50 # 50 is the maximum
-
-collect_data_online = True
-
-# Step1: using zip code searching for all restaurants and find their id
-# Example: https://api.citygridmedia.com/content/places/v2/search/where?type=restaurant&where=90045&publisher=xxxx
-# Step2: using id to find all reviews for that restaurant
-# Example: https://api.citygridmedia.com/content/reviews/v2/search/where?listing_id=44537579&publisher=xxxx
-# Note: searching is based on radius on distance, which means using zip does not mean all the result restaurants'
-# addresses match the zip,
-
-# county id 1001 Autauga County
-# zips = [36003, 36006, 36008, 36051, 36066, 36067, 36068, 36749]
-
-# Info: sqlite date: https://www.sqlite.org/lang_datefunc.html
-
-def initDataBase(c):
-    SetupDataBase.createTables(c);
-    SetupDataBase.buildCountiesTable(c);
-    SetupDataBase.buildStatesTable(c);
-
-def getCountyID(c, state, county):
-    comm = 'SELECT c.id from counties as c, states as s where c.state_id = s.id and'\
-            ' s.code = \"%s\" and c.name = \"%s\"' % (state, county)
-    c.execute(comm)
-    result = c.fetchone()
-    if result:
-        return result[0]
-    else:
-        print("Cannot find county id!")
-        exit(0)
-
-
-
-if not os.path.exists(restaurant_file_folder):
-    os.makedirs(restaurant_file_folder)
-if not os.path.exists(review_file_folder):
-    os.makedirs(review_file_folder)
-
-
-zips = RestaurantCollector.getZips(query_state, query_county)
-print zips
-# save all restaurants information to Json files according to their zip code
-# If interrupted, try restart from a certain zip code
-if collect_data_online:
-    RestaurantCollector.queryAndSaveRestaurantsJsonFiles(zips, restaurant_file_folder, query_size, publisher)
-
 conn = sqlite3.connect('dva.db')
 c = conn.cursor()
-#initDataBase(c) # call only when recreating the database from empty
+#initDataBase(c)
+#c.execute('select county_id, Asian+African_American+White+American_Indian+Pacific_Islander as'\
+#          'population from year_2013 order by population desc;')
+#result = c.fetchone()
+#while result:
 
-# Parse the restaurants Json file and insert contents into db
-county_id = getCountyID(c, query_state, query_county)
-print('county id: ' + str(county_id))
-for file_name in listdir(restaurant_file_folder):
-    with open(restaurant_file_folder + file_name) as json_file:
-        data = json.load(json_file)
-        zip = int(file_name[0:file_name.find('-')])
-        RestaurantJsonFileParser.parseRestaurantJsonFiles(c, data, zip, county_id)
-        print("Finish parsing " + file_name)
+# for row in c.execute('select a.code, b.name from states as a, counties as b where a.id = b.state_id;'):
+#
+#     state = row[0]
+#     county = row[1]
+#     zip = RestaurantCollector.getZips(state, county);
+#     if len(zip) == 0:
+#         print state + " " + county
 
-# Gather the restaurant list and get reviews for each one, then save them to Json files
-if collect_data_online:
-    ReviewCollector.queryAndSaveReviewJsonFiles(c, review_file_folder, county_id, query_size, publisher)
-
-# Parse the reviews Json file and insert contents into db
-for file_name in listdir(review_file_folder):
-    with open(review_file_folder + file_name) as json_file:
-        data = json.load(json_file)
-        zip = int(file_name[0:file_name.find('-')])
-        ReviewJsonFileParser.parseReviewJsonFiles(c, data)
-        print("Finish parsing " + file_name)
 
 conn.commit()
 conn.close()
+
+
--- SetupDataBase.py
+++ SetupDataBase.py
@@ -13,26 +13,94 @@ def createTables(c):
               'FOREIGN KEY(tag_id) REFERENCES tags(id))')
     c.execute('create table reviews (id TEXT PRIMARY KEY NOT NULL, restaurant_id integer,'\
               'rating integer, date text, FOREIGN KEY(restaurant_id) REFERENCES restaurants(id))')
+    c.execute('create table zips (zip INT PRIMARY KEY NOT NULL, county_id integer, '\
+              'FOREIGN KEY(county_id) REFERENCES counties(id))')
 
 def buildCountiesTable(c):
-    with open('counties.txt', 'rb') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='\t')
+    id = 1;
+    with open('all_2013.csv', 'rb') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
         state_id = 1
         for row in reader:
-            id = int(row['id'])
-            if id % 1000 == 0:
-                state_id = id / 1000
-                continue
-            comm = 'INSERT OR IGNORE INTO counties VALUES (%d, \"%s\", %d)' % (id, row['name'], state_id)
+            state = row['State'][1:]
+            state_id = getStateID(c, state)
+            comm = 'INSERT OR IGNORE INTO counties VALUES (%d, \'%s\', %d)' % (id, row['County'].lower(), state_id)
             print comm
             c.execute(comm)
+            id = id + 1
 
 def buildStatesTable(c):
     with open('states.txt', 'rb') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
             id = int(row['id'])
-            comm = 'INSERT INTO states VALUES (%d, \"%s\", \"%s\")' % \
+            comm = 'INSERT INTO states VALUES (%d, \'%s\', \'%s\')' % \
                 (int(row['id']), row['code'], row['name'])
             print comm
             c.execute(comm)
+
+def getStateID(c, state) :
+    if len(state) == 2:
+        comm = 'SELECT id from states where code = \'%s\'' % state
+    else:
+        comm = 'SELECT id from states where name = \'%s\'' % state
+    print comm
+    c.execute(comm)
+    result = c.fetchone()
+    if result:
+        return result[0]
+    else:
+        print("Cannot find state id! %s" % (state))
+        exit(0)
+
+
+def getCountyID(c, state, county):
+    #print state+' '+county
+    county = county.lower()
+    if len(state) == 2:
+        comm = 'SELECT c.id from counties as c, states as s where c.state_id = s.id and'\
+                ' s.code = \'%s\' and c.name = \'%s\'' % (state, county)
+    else:
+        comm = 'SELECT c.id from counties as c, states as s where c.state_id = s.id and'\
+                ' s.name = \'%s\' and c.name = \'%s\'' % (state, county)
+    c.execute(comm)
+    result = c.fetchone()
+    if result:
+        return result[0]
+    else:
+        print("Cannot find county id! %s %s" % (state, county))
+        return -1
+
+def buildDemoTableAtYear(c, year):
+    c.execute('create table year_%d (county_id PRIMARY KEY NOT NULL, state TEXT, county_name text, '\
+              'Asian integer, African_American integer, White integer, American_Indian integer, '\
+              'Pacific_Islander integer, FOREIGN KEY(county_id) REFERENCES counties(id))' % year)
+    file_name = "all_" + str(year) + ".csv"
+    with open(file_name, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            state = row['State'][1:]
+            county = row['County']
+            county_id = getCountyID(c, state, county)
+            if county_id < 0:
+                continue
+            comm= 'INSERT OR IGNORE INTO year_%d VALUES(%d, \'%s\', \'%s\', %d, %d, %d, %d, %d)' % \
+                  (year, county_id, state, county, int(row['Asian'].replace(',','')), \
+                   int(row['Black or African American'].replace(',','')),int(row['White'].replace(',','')), \
+                   int(row['American Indian and Alaska Native'].replace(',','')), \
+                   int(row['Native Hawaiian and Other Pacific Islander'].replace(',','')))
+            print comm
+            c.execute(comm)
+
+def buildZipTable(c):
+    with open('ZIP_CODES.csv', 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            state = row['State']
+            county = row['County']
+            county_id = getCountyID(c, state, county);
+            comm = 'INSERT INTO zips VALUES (%d, %d)' % (int(row['Zip']), county_id)
+
+def buildDemoTables(c):
+    for year in [2010, 2011, 2012, 2013]:
+        buildDemoTableAtYear(c, year)
